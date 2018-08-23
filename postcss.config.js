d7ebac16d9973607786fd46457f8ff3905614828
Ethan Martin <Martin.Ethan@pennmutual.com>
2018-08-23 11:07:48 -0400

    Better feature detection, and warning the user why the demos aren't rendering on IE/Edge. Fix elm logo too.

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: [
+    require('postcss-import'),
+    require('postcss-custom-properties'),
+    require('postcss-calc'),
+    require('postcss-discard-duplicates'),
+  ],
+}
