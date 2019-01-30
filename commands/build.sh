137b1d767e0c98bfc0fc9b6c733aa40a186ee4f1
Ethan Martin <Martin.Ethan@pennmutual.com>
2019-01-29 22:24:59 -0500

    Clean up dependency stuff. Separate out dev stuff.

--- commands/build.sh
+++ commands/build.sh
@@ -1,4 +1,4 @@
 # meant to be run from the base of this project
 
-./node_modules/.bin/postcss --config postcss.config.js -o card-animation/public/css/explanation.css card-animation/public/css/src/explanation.css
-./node_modules/.bin/postcss --config postcss.config.js -o card-animation/public/css/iterations.css card-animation/public/css/src/iterations.css
+dev/node_modules/.bin/postcss --config postcss.config.js -o card-animation/public/css/explanation.css card-animation/public/css/src/explanation.css
+dev/node_modules/.bin/postcss --config postcss.config.js -o card-animation/public/css/iterations.css card-animation/public/css/src/iterations.css
--- web-component/pilatch-card/bs-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  server: {
-    baseDir: './',
-    index: 'demo/index.html',
-  },
-  open: false,
-  notify: false
-}
