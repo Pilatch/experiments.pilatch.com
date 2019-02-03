a6ac3cbb3250fb3b14ac976932e064ab501e53fa
Ethan Martin <Martin.Ethan@pennmutual.com>
2019-02-03 18:48:51 -0500

    Make the homepage for the react-app known to the build process. Add the build folder so we can serve it without building on my lil' server.

---
+++ card-animation/frameworks/react-app/build/precache-manifest.23c9a2baf6c841b12d210b6b84d4a215.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7005506dcd05a6ec56e3",
+    "url": "/card-animation/frameworks/react-app/build/static/js/runtime~main.7005506d.js"
+  },
+  {
+    "revision": "0dffe774704bf51db5e5",
+    "url": "/card-animation/frameworks/react-app/build/static/js/main.0dffe774.chunk.js"
+  },
+  {
+    "revision": "5cb9b55e9859431b45ee",
+    "url": "/card-animation/frameworks/react-app/build/static/js/1.5cb9b55e.chunk.js"
+  },
+  {
+    "revision": "0dffe774704bf51db5e5",
+    "url": "/card-animation/frameworks/react-app/build/static/css/main.78386b58.chunk.css"
+  },
+  {
+    "revision": "1557fe8b8d1c40081722a43f5aa09f6a",
+    "url": "/card-animation/frameworks/react-app/build/index.html"
+  }
+];
