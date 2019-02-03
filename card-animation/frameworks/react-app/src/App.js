b8265ce67677bd2a25720af193194dec0ea7b92b
Ethan Martin <Martin.Ethan@pennmutual.com>
2019-02-02 20:42:59 -0500

    Really simple react implementation with setInterval, and not shifting the rest of the cards in the hand around when a card is placed. This looks correct, but to be full-featured, we'll need to see how that shift looks.

--- card-animation/frameworks/react-app/src/App.js
+++ card-animation/frameworks/react-app/src/App.js
@@ -1,17 +1,45 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      placedCardIndex: -1
+    }
+    setInterval(() => {
+      this.setState({
+        placedCardIndex: this.state.placedCardIndex === 4
+          ? 0
+          : this.state.placedCardIndex + 1
+      })
+    }, 1250)
+  }
+
   render() {
     return (
-      <div className="App">
-        <pilatch-card suit="rock" rank="1" up></pilatch-card>
-        <pilatch-card suit="rock" rank="2" up></pilatch-card>
-        <pilatch-card suit="rock" rank="3" up></pilatch-card>
-        <pilatch-card suit="rock" rank="4" up></pilatch-card>
-        <pilatch-card suit="rock" rank="5" up></pilatch-card>
-      </div>
+      <main>
+        <div class="table-top">
+          <Hand placed={this.state.placedCardIndex}/>
+          <pilatch-card nothing class="player-placed-card-area"></pilatch-card>
+        </div>
+      </main>
     );
+  }
+}
+
+class Hand extends Component {
+  render() {
+    return (
+      [0,1,2,3,4].map(index => {
+        let cssClass = `player-hand hand-size-5 card-${index} ${
+          this.props.placed === index
+            ? 'player-placed-card-area'
+            : ''
+        }`
+        return <pilatch-card suit="rock" rank={index + 1} up class={cssClass}></pilatch-card>
+      })
+    )
   }
 }
 
