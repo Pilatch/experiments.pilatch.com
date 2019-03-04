634a23e4e791d8949374c1761d01a5f28079507f
Ethan Martin <Martin.Ethan@pennmutual.com>
2019-03-03 22:32:55 -0500

    derp?

--- card-animation/build.sh
+++ card-animation/build.sh
@@ -1,3 +1,4 @@
 cd source/elm
-elm-make Explanation/Main.elm Explanation/Naive.elm --output ../../public/js/Elm.Explanation.js
-elm-make Iterations/Main.elm --debug --output ../../public/js/Elm.Iterations.js
+alias make='../../../dev/node_modules/.bin/elm-make'
+$make Explanation/Main.elm Explanation/Naive.elm --output ../../public/js/Elm.Explanation.js
+$make Iterations/Main.elm --debug --output ../../public/js/Elm.Iterations.js
