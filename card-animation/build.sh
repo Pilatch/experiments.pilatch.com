ccf2f1c1e4114bf125280fa833b259ef5a118e47
Ethan B. Martin <ethan@pilatch.com>
2019-10-06 14:11:39 -0400

    Got naive back in there

--- card-animation/build.sh
+++ card-animation/build.sh
@@ -1,4 +1,3 @@
 cd source/elm
-npx elm make Explanation/Main.elm --debug --output ../../public/js/Elm.Explanation.js
-#npx elm make Explanation/Naive.elm --output ../../public/js/Elm.Explanation.js
+npx elm make Explanation/Main.elm Explanation/Naive.elm --output ../../public/js/Elm.Explanation.js
 npx elm make Iterations/Main.elm --debug --output ../../public/js/Elm.Iterations.js
